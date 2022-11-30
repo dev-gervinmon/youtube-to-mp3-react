b8a458cd9785d0c42b3c10e11366c2c37d74720c
dev-gervinmon <dev.montibon.geraldvincent54@gmail.com>
2022-12-01 00:19:33 +0800

    added loading screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
 import Main from './pages/Main';
+import LoadingScreen from './components/LoadingScreen';
+import { useState } from 'react';
 
 function App() {
-  return <Main/>
+  const [isLoading, setIsLoading] = useState(true)
+
+  if (isLoading) {
+    setTimeout(() =>{
+      setIsLoading(false)
+    }, 2000)
+  }
+
+  return (
+    <>
+      {isLoading ?
+        <div>
+          <LoadingScreen/>
+          <div className='invisible'><Main/></div>
+        </div>:
+        <Main/>
+      }
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,18 @@
+import '../App.css'
+
+export default function LoadingScreen() {
+    return(
+        <div class="center">
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+        </div>
+    )
+}
